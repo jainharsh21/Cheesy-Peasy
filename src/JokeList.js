23a502c627455d43f172ff11dcae3e2ce0513dcf
HARSH JAIN <jainharsh270@gmail.com>
2020-04-23 17:42:08 +0530

    create joke list component/added axios depenedency

---
+++ src/JokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class JokeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>JokeList</h1>
+      </div>
+    );
+  }
+}
+
+export default JokeList;
