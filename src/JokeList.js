7f5d4811bc90f43e1e81d210426ef9e56c698a3d
HARSH JAIN <jainharsh270@gmail.com>
2020-04-23 17:57:28 +0530

    getting random jokes and rendering them

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokes: 10,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: [],
+    };
+  }
+  async componentDidMount() {
+    const API_URL = "https://icanhazdadjoke.com/";
+    let jokes = [];
+    while (jokes.length < this.props.numJokes) {
+      let res = await axios.get(API_URL, {
+        headers: { Accept: "application/json" },
+      });
+      jokes.push(res.data.joke);
+    }
+    // console.log(jokes);
+    this.setState({
+      jokes: jokes,
+    });
+  }
   render() {
     return (
-      <div>
-        <h1>JokeList</h1>
+      <div className="JokeList">
+        <h1>Dad Jokes</h1>
+        <div className="JokeList-jokes">
+          {this.state.jokes.map((j) => (
+            <div>{j}</div>
+          ))}
+        </div>
       </div>
     );
   }
